80d6151d398b3facc6326355567c3ade38ff7a8b
Carlos Renan <162735531+Joelho456@users.noreply.github.com>
2024-05-11 20:54:17 +0000

    Endpoint Read All [GET] / Animal implemantado

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+const lista = ['Cachorro','Gato','Lagarta']
+
+//endpoint Read All [GET] / Animal
+app.get('/animal', function (req,res){
+  res.send(lista)
+})
+
 app.listen(3000, function (req, res){
   console.log("Carregando http://localhost:3000/")
 })
