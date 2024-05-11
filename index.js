9fa4d33690aaeb3775e74f9e357b88d9ff1fd91b
Carlos Renan <162735531+Joelho456@users.noreply.github.com>
2024-05-11 23:04:09 +0000

    Endpoint Read By ID "[GET] /animal/:id" criado

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ app.get('/animal', function (req,res){
   res.send(lista)
 })
 
+// Endpoint Read By ID [GET] /animal/:id
+app.get('/animal/:id',function (req,res){
+
+  // Acessando o parâmetro da rota id
+  const id = req.params.id
+
+  // Acessa o item na lista usando o ID -1
+  const item = lista[id - 1]
+
+ // Enviando o item como resposta
+  res.send(item)
+})
+
 app.listen(3000, function (req, res){
   console.log("Carregando http://localhost:3000/")
 })
