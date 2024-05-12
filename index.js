d784d6e28a0727d215d988303544990466802d7c
Carlos Renan <162735531+Joelho456@users.noreply.github.com>
2024-05-12 00:42:20 +0000

    Imprementação do Create "[POST]  /animal"

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
 
 const lista = ['Cachorro','Gato','Lagarta']
 
-//endpoint Read All [GET] / Animal
+//endpoint Read All [GET] /animal
 app.get('/animal', function (req,res){
   res.send(lista)
 })
@@ -24,6 +24,24 @@ app.get('/animal/:id',function (req,res){
  // Enviando o item como resposta
   res.send(item)
 })
+
+// Sinalizando para o Express que estamos usando JSON no Body
+app.use(express.json())
+
+// Endpoint Create [POST] /animal
+app.post('/animal', function (req, res){
+  // Acessando o Body da Requisição
+   const body = req.body
+
+   // Acessamos a propriedade 'nome' do body
+   const novoItem = body.nome
+
+   // Adicionamos na lista
+   lista.push(novoItem)
+
+   // Exibimos uma mensagem de sucesso
+  res.send('Item adicionado com sucesso: ' + novoItem)
+  })
 
 app.listen(3000, function (req, res){
   console.log("Carregando http://localhost:3000/")
